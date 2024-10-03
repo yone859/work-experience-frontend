c39711262a55ce4b671663fffc279af11235c81b
YoneTom <yonetomo.asics@gmail.com>
2024-10-03 14:30:10 +0900

    経歴登録画面レイアウトファイル修正

--- src/app/register-career/page.js
+++ src/app/register-career/page.js
@@ -1,11 +1,42 @@
+"use client";
+import { Label, TextInput, Select, Textarea, Checkbox,  } from "flowbite-react";
+import { useState } from 'react';
+import RegisterForm from '@/components/Career/registerForm';
+import { fetchCreateCareer } from '@/lib/registerCareerAPI';
 
-export default function Home() {
 
+export default  function Layout({children}) {
 
+  const [form, setForm] = useState("");
+  // ↑親コンポーネントで使う：formの初期値とformを更新する関数を宣言
+  // ↓子コンポーネントから受け取った値で親コンポーネントのformを更新する関数A
+  const handleFormChange = (newForm) => {
+    setForm(newForm);
+  };
+
+  const register = async () => {
+    try {
+      const workExp = await fetchCreateCareer(form);
+      setWorkExp(workExp);
+      setCareerList(workExp.career);
+      setCareerListLength(workExp.career.length);
+      workExp.career.length==0? setIsNoMoreCareer(true) : setIsNoMoreCareer(false);
+
+  } catch (error) {
+      console.error("Failed to fetch user list:", error);
+  }
+  };
 
   return (
     <>
-<div></div>
-   </>
+      <div>
+        {/* <button type="button" onClick={show}>送信</button> */}
+        <form className="w-3/5 mx-auto">
+        <RegisterForm handleFormChange={handleFormChange} />
+          <button type="button" onClick={register}>登録</button>
+        </form>
+      </div>
+{children}
+    </>
   );
 }

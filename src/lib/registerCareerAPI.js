bd968521e20b885739131e306713dc55f83cfcb8
YoneTom <yonetomo.asics@gmail.com>
2024-10-03 17:38:13 +0900

    経歴登録APIファイル追加

--- src/lib/registerCareerAPI.js
+++ src/lib/registerCareerAPI.js
@@ -1,30 +1,35 @@
-// import prisma from './prisma';
+  export async  function fetchCreateCareer(form) {
 
+    const body = new FormData()
+    body.append("name", "tanaka")
+    body.append("age", 12)
 
-
-  export async  function fetchWorkExpAPI(careerListLength=0) {
+  const data = {
+      method: 'POST',
+      cache: 'no-store',
+      body: body,
+      
+  }
     
   const res = await fetch(
-    `http://localhost:3000/top?current_record=${careerListLength+1}&next_fetch_record=${careerListLength+3}`, {
-    cache: 'no-store',
-  })
+    `http://localhost:3000/create-work-experience`, data)
   .then(response => {
     // レスポンスが成功かどうか確認
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    // レスポンスを JSON として返す
-    return response.json();
+
 
   })
-  .then(data => {
-    // 取得したデータを処理
-    return  data;
-  })
+  // .then(data => {
+  //   // 取得したデータを処理
+  //   return  data;
+  // })
   .catch(error => {
     // エラーハンドリング
     console.error('API取得中にエラーが発生しました:', error);
   });
-  return fetchWorkExpAPI2(res)
+  console.log("成功")
+  return 
 
   }

82eb820d0c72529a5c2854c0372a8cbaa110f5d3
YoneTom <yonetomo.asics@gmail.com>
2024-10-04 13:07:56 +0900

    資格日時形式修正（"yyyy年mm月dd日"→"yyyy年mm月"）

--- src/components/Career/Qualification.js
+++ src/components/Career/Qualification.js
@@ -6,6 +6,24 @@ import { Suspense } from 'react'
 const fnt = Inconsolata({ subsets: ['latin'] })
 
 export default  function Qualification({ Qualification }) {
+
+  function parseCustomDate(dateStr) {
+    // "yyyy年mm月dd日" の形式から "yyyy-mm-dd" 形式に変換
+    const formattedDateStr = dateStr.replace(/年|月/g, '-').replace(/日/g, '');
+    return new Date(formattedDateStr);
+}
+
+function getYearMonth(dateStr) {
+    // 日付文字列をDateオブジェクトに変換
+    const date = parseCustomDate(dateStr);
+  
+    // 年と月をそれぞれ取得
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1; // getMonth()は0-11の範囲を返す
+  
+    // トータルの月数の差を返す(開始月も期間に含めるため１加える)
+    return year + '年' + month + '月' 
+  }
   return ( 
     <>
       <div className="ml-7 bg-self-bgcollar w-2/5  pt-4 pb-4 pr-4">
@@ -14,7 +32,7 @@ export default  function Qualification({ Qualification }) {
           </div>
           <div className="pl-8">
             {Qualification?.map((obj,i) => (
-              <li  key={i + 1} >{obj.name} ({obj.obtainment_date})</li>
+              <li  key={i + 1} >{obj.name} ({getYearMonth(obj.obtainment_date)})</li>
             ))}
           </div>
       </div>
